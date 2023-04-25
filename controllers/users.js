bbd5c4901f14b5518ad7042661208912d7e6da77
afbelardi <afbelardi@gmail.com>
2023-04-25 19:09:53 -0400

    user schema file updated

--- controllers/users.js
+++ controllers/users.js
@@ -6,97 +6,103 @@ const userRouter = express.Router();
 const verifyJWT = require('../middleware/verifyJWT');
 
 
-
-
-
-userRouter.post("/register", async (req, res) => {
-    const user = req.body;
-
-    const takenUsername = await User.findOne({ username: user.username });
-    const takenEmail = await User.findOne({ email: user.email });
-
-    if (takenEmail || takenUsername) {
-        res.json({
-            status: 400,
-            message: "Username or email has already been taken"
-        })
-    } else {
-        user.password = await bcrypt.hash(req.body.password, 10)
-
-        const dbUser = new User({
-            username: user.username.toLowerCase(),
-            email: user.email.toLowerCase(),
-            password: user.password,
-        })
-
-        dbUser.save();
-
-       res.json({
-            status: 200,
-            message: "Success",
-            user
-        })
-    }
-});
-
-
-userRouter.post("/login", (req, res) => {
-    const userLoggingIn = req.body;
-
-    User.findOne({username: userLoggingIn.username})
-    .then(dbUser => {
-        if (!dbUser) {
-            return res.json({
-                message: "Invalid Username or Password"
-            })
-        }
-        bcrypt.compare(userLoggingIn.password, dbUser.password)
-        .then(isCorrect => {
-            if (isCorrect) {
-                const payload = {
-                    id: dbUser._id,
-                    username: dbUser.username,
-                    email: dbUser.email
-                }
-                jwt.sign(
-                    payload,
-                    process.env.JWT_SECRET,
-                    {expiresIn: 86400},
-                    (err, token) => {
-                        if (err) return res.json({message: err})
-                        res.send({
-                            status: 200,
-                            message: "Success",
-                            token: token
-                        })
-                    }
-                )
-            } else {
-                return res.json({
-                    status: 400,
-                    message: "Invalid Username or Password"
-                })
-            }
-        })
-    })
-})
-
-
-userRouter.get('/userfavorites', verifyJWT, async(req, res) => {
+userRouter.post('/signup', async (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1]
 
-        const decodedToken = await jwt.decode(token, process.env.JWT_SECRET)
-        const foundUser = await User.findById(decodedToken.id).populate("favorites")
-    
-        res.json(foundUser.favorites)
-    } catch(error){
-        res
-            .status(400)
-            .json(error)
+    } catch(error) {
+        next(error)
     }
-    
 })
+
+
+// userRouter.post("/register", async (req, res) => {
+//     const user = req.body;
+
+//     const takenUsername = await User.findOne({ username: user.username });
+//     const takenEmail = await User.findOne({ email: user.email });
+
+//     if (takenEmail || takenUsername) {
+//         res.json({
+//             status: 400,
+//             message: "Username or email has already been taken"
+//         })
+//     } else {
+//         user.password = await bcrypt.hash(req.body.password, 10)
+
+//         const dbUser = new User({
+//             username: user.username.toLowerCase(),
+//             email: user.email.toLowerCase(),
+//             password: user.password,
+//         })
+
+//         dbUser.save();
+
+//        res.json({
+//             status: 200,
+//             message: "Success",
+//             user
+//         })
+//     }
+// });
+
+
+// userRouter.post("/login", (req, res) => {
+//     const userLoggingIn = req.body;
+
+//     User.findOne({username: userLoggingIn.username})
+//     .then(dbUser => {
+//         if (!dbUser) {
+//             return res.json({
+//                 message: "Invalid Username or Password"
+//             })
+//         }
+//         bcrypt.compare(userLoggingIn.password, dbUser.password)
+//         .then(isCorrect => {
+//             if (isCorrect) {
+//                 const payload = {
+//                     id: dbUser._id,
+//                     username: dbUser.username,
+//                     email: dbUser.email
+//                 }
+//                 jwt.sign(
+//                     payload,
+//                     process.env.JWT_SECRET,
+//                     {expiresIn: 86400},
+//                     (err, token) => {
+//                         if (err) return res.json({message: err})
+//                         res.send({
+//                             status: 200,
+//                             message: "Success",
+//                             token: token
+//                         })
+//                     }
+//                 )
+//             } else {
+//                 return res.json({
+//                     status: 400,
+//                     message: "Invalid Username or Password"
+//                 })
+//             }
+//         })
+//     })
+// })
+
+
+// userRouter.get('/userfavorites', verifyJWT, async(req, res) => {
+//     try {
+//         const token = req.headers.authorization.split(' ')[1]
+
+//         const decodedToken = await jwt.decode(token, process.env.JWT_SECRET)
+//         const foundUser = await User.findById(decodedToken.id).populate("favorites")
+    
+//         res.json(foundUser.favorites)
+//     } catch(error){
+//         res
+//             .status(400)
+//             .json(error)
+//     }
+    
+// })
 
 
 
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Park = require("./Park");
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
     username: {
@@ -20,7 +21,27 @@ const userSchema = mongoose.Schema({
     favorites: [{
         type: mongoose.Schema.Types.ObjectId, ref: 'Park'
     }]
-}, {timestamps: true})
+}, {timestamps: true});
+
+userSchema.pre('save', async function (next) {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+        this.password = hashedPassword;
+        next();
+    } catch (error) {
+        next(error)
+    }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+    try {
+        const isMatch = await bcrypt.compare(password, this.password);
+        return isMatch;
+    } catch (error) {
+        next(error);
+    }
+}
 
 const User = mongoose.model("User", userSchema)
 
